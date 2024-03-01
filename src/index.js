0cd632a6010416b4371e3473f0302ae078cd3a9b
hadeszhangg <truongdangminhtan333@gmail.com>
2024-03-01 19:58:13 +0700

    Update setup struchure files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './Views/App';
 import reportWebVitals from './reportWebVitals';
-
+import './Styles/global.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
